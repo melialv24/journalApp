e11ad67e14459ea8b1b40387b17182763bb5a3e8
melissa alvarez <melissaalh24@gmail.com>
2022-08-11 22:02:00 -0500

    primeros avances con  redux

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,32 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { Button, IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   //el typografy se le puede poner el component y variant
   // el variant le pone los estilos que materialUI le pone por defecto
   return (
     <JournalLayout>
-      <Typography component="h1">JournalPage</Typography>
+      {/*<Typography component="h1">JournalPage</Typography>
+       */}
+      <NothingSelectedView />
+
+      {/** <NoteView />*/}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import {JournalApp} from './JournalApp'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { JournalApp } from "./JournalApp";
+import { Provider } from "react-redux";
+import "./styles.css";
+import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-)
+);
