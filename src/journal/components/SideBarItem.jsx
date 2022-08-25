b9536478590141888db203b20e0787dfaad67fc0
Melissa <malvarez@datagas.com.co>
2022-08-25 11:43:42 -0500

    activando las notas

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,4 +1,5 @@
 import { TurnedInNot } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import {
   Grid,
   ListItem,
@@ -7,15 +8,22 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useMemo } from "react";
+import { setActiveNote } from "../../store/journal/journalSlice";
 
-export const SideBarItem = ({ id, title, body }) => {
+export const SideBarItem = ({ id, title, body, date, imageUrls = [] }) => {
+  const dispatch = useDispatch();
+
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
 
+  const onActiveNote = () => {
+    dispatch(setActiveNote({ id, title, body, date, imageUrls }));
+  };
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onActiveNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
